cf627dde5722dd2877f850722bc4d567abbf69c5
dima2959 <dima2959@gmail.com>
2022-06-25 14:04:20 +0300

    try1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println("Сообщение: " + bean.getMessage() + " Объект с хэшом: " + bean);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("Сообщение: " + bean1.getMessage() + " Объект с хэшом: " + bean1);
+        System.out.println("Сравнение по ссылке = " + (bean == bean1));
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сообщение: " + beanCat.getMessage() + " Объект с хэшом: " + beanCat);
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сообщение: " + beanCat1.getMessage() + " Объект с хэшом: " + beanCat1);
+        System.out.println("Сравнение по ссылке = " + (beanCat == beanCat1));
     }
 }
